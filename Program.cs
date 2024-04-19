395d07803960d81eac30b46252e994992865bbb7
Fontoura - E. Escolinha de TI - BNE <nycolasfontoura@employer.matriz>
2024-04-19 09:43:32 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Determine o valor presente:");
+double ValorPresente = double.Parse(Console.ReadLine());
+
+Console.Write("Determine a taxa de juros presente:");
+double taxaJuros = double.Parse(Console.ReadLine());
+
+Console.Write("Determine o periodo presente:");
+int periodo= int.Parse(Console.ReadLine());
+
+double valorFuturo = ValorPresente * (Math.Pow((taxaJuros/100) + 1, periodo));
+
+Console.WriteLine("O rendimento total foi: " + Math.Round(valorFuturo, 2));
